5e931b6c53a88d72bd163eea322b18c464cae463
Tim Molderez <id@timmolderez.be>
2014-07-16 18:54:23 +0200

    Implemented diagram API

---
+++ Ekeko AJFX/src/ekeko_ajfx/analysis.clj
@@ -0,0 +1,5 @@
+(ns
+  ^{:doc "Defines the frame inference algorithm"
+    :author "Tim Molderez" }
+  ekeko-ajfx.analysis
+  (:use [inspector-jay.core]))
--- Ekeko AJFX/src/ekeko_ajfx/diagram.clj
+++ Ekeko AJFX/src/ekeko_ajfx/diagram.clj
@@ -12,11 +12,12 @@
              }
            :may-be-modified { ... }
            :may-be-read { ... }
-          }
-          "
+          }"
     :author "Tim Molderez" }
   ekeko-ajfx.diagram
   (:use [inspector-jay.core]))
+
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
 "Ensures all objects within a graph have a unique identifier"
 (def last-obj-id (atom 0))
@@ -28,32 +29,75 @@
 
 (defn new-diagram [formals]
   "Create a new alias diagram, given the formal parameter names of the body to be analysed"
-  (let [objs (zipmap 
-               (for [x formals] (new-obj-id))
-               (for [x formals] #{x (str "@" x)}))
-        objs-this (assoc objs (new-obj-id) #{"this"})]
-    {:obj objs-this
+  (let [root-names (for [x formals] (keyword (str "@" x)))
+        names (for [x formals] (keyword x)) 
+        id-sets (for [x formals] #{(new-obj-id)})
+        names (zipmap
+                (concat root-names names)
+                (concat id-sets id-sets))
+        names-this (assoc names :this #{(new-obj-id)})]
+    {:names names-this
     :must-mod {}
     :may-mod {}
     :may-read {}}))
 
-(defn add-object [diagram names]
-  "Add a new object to a diagram. This object can be referred to by a given set of names."
-  (assoc diagram :obj
-    (assoc (diagram :obj)
-      (new-obj-id) names)))
+(defn add-name [diagram object name]
+  "An object can now be referred to by an additional name."
+  (let [name-key (keyword name)
+        name-set ((diagram :names) name-key)
+        new-name-set (clojure.set/union name-set #{object})
+        new-names (assoc (diagram :names) name-key new-name-set)]
+    (assoc diagram :names new-names)))
 
-(defn add-name [diagram object])
+
+(defn add-object [diagram name]
+  "Add a new object to a diagram. This object is referred to by a certain name."
+  (add-name diagram (new-obj-id) name))
 
 (defn replace-name [diagram old-name new-name]
-  )
+  "All objects that are referred to by old-name will now be referred to be new-name instead."
+  (let [new-key (keyword new-name)
+        old-key (keyword old-name)
+        new-ids (clojure.set/union
+                  ((diagram :names) new-key)
+                  ((diagram :names) old-key))
+        new-names (assoc (dissoc (diagram :names) old-key)
+                    new-key new-ids)]
+    (assoc diagram :names new-names)))
 
-(defn remove-name [diagram name])
+(defn remove-name [diagram name]
+  "Remove a name from a diagram, without removing the objects it refers to"
+  (let [new-names (dissoc (diagram :names) (keyword name))]
+    (assoc diagram :names new-names)))
 
-(defn find-objs-by-name [diagram ])
+(defn find-objs-by-name [diagram name]
+  "Find all objects that may be referred to be a certain name"
+  ((diagram :names) (keyword name)))
 
-(defn find-edges [diagram source-name field-name kinds])
+(defn find-edges [diagram source-name label kind]
+  "Retrieve the edges with a given name at the source, with a particular edge label, of a particular edge kind"
+  (let [source-ids (find-objs-by-name diagram source-name)
+        all-edges (diagram kind)
+        matching-edges (select-keys all-edges source-ids)
+        filtered (for [x matching-edges]
+                   (let [src (x first)
+                         tgts (x second)]
+                     (for [y tgts]
+                       [src y])))]
+    (mapcat identity filtered))) ; Flatten by one level
 
-(defn remove-edge [diagram source-name edge-name kinds])
+(defn add-edges [diagram source-name label kind target-name]
+  "Add edges from all nodes with source-name"
+  (let [source-ids (find-objs-by-name diagram source-name)
+        target-ids (find-objs-by-name diagram target-name)
+        all-edges (diagram kind)
+        add-edge (fn [edges src tgt]
+                   (let [new-set (conj (edges src) tgt)]
+                     (assoc edges src new-set)))
+        new-edges ()]
+    (assoc diagram kind new-edges)
+    ))
 
-(defn remove-object [diagram id])
+(defn add-edges-to-new-object [diagram source-name label kind])
+
+(defn remove-edges [diagram source-name label kind])
